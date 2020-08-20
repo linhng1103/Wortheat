4d38eea52141e8e6309c1a66a840df30da831698
linhng1103 <67526264+linhng1103@users.noreply.github.com>
2020-08-20 14:31:02 -0400

    function gg font

--- wp-content/themes/createdtheme/functions.php
+++ wp-content/themes/createdtheme/functions.php
@@ -3,4 +3,10 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'twentytwenty', get_template_directory_uri().'/style.css' );
 }
+function add_fonts() {
+ 
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Lato|Roboto&display=swap', false ); 
+}
+ 
+add_action( 'wp_enqueue_scripts', 'add_fonts' );
 ?>
